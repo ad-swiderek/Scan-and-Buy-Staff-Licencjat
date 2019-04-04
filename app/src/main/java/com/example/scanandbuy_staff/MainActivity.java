521cb3d8117ec8f43aa0abd41c53b3484061fc38
ad-swiderek <androsxix@gmail.com>
2019-04-04 13:06:20 +0200

    first commit

---
+++ app/src/main/java/com/example/scanandbuy_staff/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.scanandbuy_staff;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.adrian.scanandbuy"; //wiadomosc (kod kreskowy) do pozniejszego przeslania w intencie
+    private final Activity activity = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scan();
+    }
+
+    protected void scan() { //uruchamiam skaner kodow kreskowych
+        IntentIntegrator integrator = new IntentIntegrator(activity); //tworze specjalny intent z biblioteki zxing ktory pozwala przejsc do activity ze skanerem
+        integrator.setBeepEnabled(false); //wylaczam dzwiek skanera
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES); //skaner jest w trybie skanowania wszystkich mozliwych kodow dostepnych w bibliotece
+        integrator.initiateScan(); //inicjuje skanowanie
+        integrator.setOrientationLocked(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data); //zapisuje rezultat naszego skanowania
+        if (result != null) { //jezeli udalo sie zeskanowac i wczesniej wcisnieto przycisk "pracownik" to przechodze do activity w ktorym dodaje produkty i przekazuje w intencie moj numer z kodu kreskowego
+            Intent intent = new Intent(this, AddProductActivity.class);
+            String message = result.getContents().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
