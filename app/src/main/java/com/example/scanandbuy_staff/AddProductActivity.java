ddcd21b37a86a4a4ef448f1820527b4a0caf1f88
ad-swiderek <37022061+ad-swiderek@users.noreply.github.com>
2019-09-01 22:38:59 +0200

    poprawki wizulane

--- app/src/main/java/com/example/scanandbuy_staff/AddProductActivity.java
+++ app/src/main/java/com/example/scanandbuy_staff/AddProductActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
